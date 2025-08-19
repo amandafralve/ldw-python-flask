e5b846fac4efb9f3e900bb9e8113e536363eb174
amandafralve <amanda.fralve@gmail.com>
2025-08-18 22:17:17 -0300

    add: games route

--- aula-01-introducao-flask/app.py
+++ aula-01-introducao-flask/app.py
@@ -9,14 +9,23 @@ app = Flask(__name__, template_folder='views') # __name__ representa o nome da a
 
 @app.route('/')
 def home(): # Função que será executada ao acessar a rota
-        return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/games')
-def home():
-        return render_template('games.html')
+def games():
+    title = 'Tarisland'
+    year = 2022
+    category = 'MMORGP'
+    # Lista em Python (array)
+    players = ['Yan', 'Ferrari', 'Valéria', 'Amanda']
+    # Dicionário em Python (objeto)
+    console = {'name': 'Playstation 5', 'manufacturer': 'Sony', 'year': 2020}
+    return render_template('games.html', title=title, year=year, category=category, players=players)
+
 
 # Se for executado diretamente pelo interpretador, inicia servidor
 if __name__ == '__main__':
     # Iniciando o servidor
     app.run(host="localhost", port=5000, debug=True)
+
