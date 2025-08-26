649b32c6238c6d93f548453641c20b446dd4ebbf
amandafralve <amanda.fralve@gmail.com>
2025-08-25 22:12:58 -0300

    add: static folder+bootstrap

--- aula-02-mvc-controllers/controllers/routes.py
+++ aula-02-mvc-controllers/controllers/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 
 def init_app(app):
     # Definindo a rota principal da aplicação '/'
@@ -26,6 +26,7 @@ def init_app(app):
             # Coletando otexto do input 
             if request.form.get('player'): #Se a caixa player for != de vazio, a lista de jogadores será chamada
                 players.append(request.form.get('player'))
+                return redirect(url_for('games'))
 
         # Dicionário em Python (objeto)
         console = {'name': 'Playstation 5', 'manufacturer': 'Sony', 'year': 2020}
@@ -41,4 +42,5 @@ def init_app(app):
             if request.form.get('title') and request.form.get('year') and request.form.get('category'):
                 gamelist.append({'Título': request.form.get('title'),'Ano': request.form.get('year'), 
                                 'Categoria': request.form.get('category')})
+                return redirect(url_for('newgame'))
         return render_template('newGame.html', gamelist=gamelist)
